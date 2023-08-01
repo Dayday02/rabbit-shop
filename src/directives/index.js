d6901b7134eebb018b9db675d74168b72cfa966b
“dayday” <1779314194@qq.com>
2023-08-01 13:55:04 +0800

    测试

--- src/directives/index.js
+++ src/directives/index.js
@@ -14,7 +14,7 @@ export const lazyPlugin = {
           ([{ isIntersecting }]) => {
             console.log(isIntersecting)
             if (isIntersecting) {
-              // 进入视口区域
+              // 进入视口区域.
               el.src = binding.value
               stop()
             }
